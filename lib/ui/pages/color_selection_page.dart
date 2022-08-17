35a59e49dce5daff1b1f8a51a33e0d9d87b8d9fe
Jesus Daniel Angulo Rivera <angulojd@uninorte.edu.co>
2022-08-17 11:47:33 -0500

    Widget sin estado

--- lib/ui/pages/color_selection_page.dart
+++ lib/ui/pages/color_selection_page.dart
@@ -25,12 +25,50 @@ class ColorSelectionPage extends StatelessWidget {
           ],
         ),
         // https://colorhunt.co/
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              ColorPalette(baseColor: 'C21010',)
-              ]),
-              ));
+        body: ListView(
+          children: [
+            ColorPalette(
+              baseColor:  'F65A83',
+              baseColor1: 'FF87B2',
+              baseColor2: '42032C',
+              baseColor3: 'D36B00',
+              baseColor4: 'E6D2AA',
+              callback: showColor,
+            ),
+            ColorPalette(
+              baseColor:  '87805E',
+              baseColor1: 'B09B71',
+              baseColor2: 'D8CCA3',
+              baseColor3: 'EDDFB3',
+              baseColor4: 'FFEE63',
+              callback: showColor,
+            ),
+            ColorPalette(
+              baseColor:  '9EB23B',
+              baseColor1: 'C7D36F',
+              baseColor2: 'FCF9C6',
+              baseColor3: 'E0DECA',
+              baseColor4: 'D4D925',
+              callback: showColor,
+            ),
+            ColorPalette(
+              baseColor:  'F7EC09',
+              baseColor1: '3EC70B',
+              baseColor2: '3B44F6',
+              baseColor3: 'A149FA',
+              baseColor4: 'FF5B00',
+              callback: showColor,
+            ),
+            ColorPalette(
+              baseColor:  '820000',
+              baseColor1: 'B9005B',
+              baseColor2: 'FF7C7C',
+              baseColor3: 'FEE0C0',
+              baseColor4: '990000',
+              callback: showColor,
+            )
+          ],
+        ));
   }
 
   // está es la función que será llamada con cada click a un ColorPalette
--- lib/ui/widgets/color_palette.dart
+++ lib/ui/widgets/color_palette.dart
@@ -6,14 +6,23 @@ class ColorPalette extends StatelessWidget {
     Key? key,
     // así se reciben en el constructor, por ejemplo
     required this.baseColor,
+    required this.baseColor1,
+    required this.baseColor2,
+    required this.baseColor3,
+    required this.baseColor4,
+    required this.callback
   }) : super(key: key);
 
   // atributos de la clase, por ejemplo
   final String baseColor;
+  final String baseColor1;
+  final String baseColor2;
+  final String baseColor3;
+  final String baseColor4;
 
   // también se puede tener como un atributo un callback, necesiario ya que es un
   // widget sin estado, por ejemplo
-  //final Function(String) callback;
+  final Function(String) callback;
 
   @override
   Widget build(BuildContext context) {
@@ -21,12 +30,48 @@ class ColorPalette extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       // vamos a mostrar los elementos en un row
-      child: 
-      Row(children: [
-        Expanded(
-          child: GestureDetector(
-          child: Container(
-            height: 100, color: ColorUtils.FromHex(baseColor),)),)
+      child: Row(
+        children: [ 
+          Expanded(
+            flex: 6,
+            child: GestureDetector(
+              onTap: (() => callback(baseColor)),
+                child: Container(
+                height: 100, color: ColorUtils.FromHex(baseColor),
+                )
+              )),
+          Expanded(
+            flex: 5,
+            child: GestureDetector(
+              onTap: (() => callback(baseColor1)),
+              child: Container(
+              height: 100, color: ColorUtils.FromHex(baseColor1),
+                )
+              )),
+          Expanded(
+            flex: 4,
+            child: GestureDetector(
+              onTap: (() => callback(baseColor1)),
+              child: Container(
+              height: 100, color: ColorUtils.FromHex(baseColor2),
+                )
+              )),
+          Expanded(
+            flex: 3,
+            child: GestureDetector(
+              onTap: (() => callback(baseColor1)),
+              child: Container(
+              height: 100, color: ColorUtils.FromHex(baseColor3),
+                )
+              )),
+          Expanded(
+            flex: 2,
+            child: GestureDetector(
+              onTap: (() => callback(baseColor1)),
+              child: Container(
+              height: 100, color: ColorUtils.FromHex(baseColor4),
+                )
+              ))
       ],)
     );
   }
